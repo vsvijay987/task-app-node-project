87ae1b120b9f03a1caa1d3b85c4bb8dac29692bd
vsvijay987 <vsvijju987@gmail.com>
2021-04-13 22:20:52 +0530

    Resource updating endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: true,
 });
 
 // const User = mongoose.model("User", {
