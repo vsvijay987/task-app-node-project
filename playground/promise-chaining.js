0bb629c58a6fd35d4ff05353da6764b6214ee24b
vsvijay987 <vsvijju987@gmail.com>
2021-04-13 21:15:23 +0530

    integrating async/await to index.js

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,9 +2,17 @@ require('../src/db/mongoose');
 
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete("60751f89c9947d4a4c764909").then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((count) => {
-    console.log(count)
-}).catch(e => console.log(e))
+// Task.findByIdAndDelete("60751f89c9947d4a4c764909").then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((count) => {
+//     console.log(count)
+// }).catch(e => console.log(e))
+
+const deleteTaskAndCountIncomplete = async(id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false});
+    return count;
+}
+
+deleteTaskAndCountIncomplete("6073da889c917806e876c122").then(count => console.log(count)).catch(e => console.log(e))
--- src/index.js
+++ src/index.js
@@ -10,54 +10,102 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    
-    user.save().then(() => {
+
+    try{
+        await user.save();
         res.status(201).send(user)
-    }).catch(err => {
-        res.status(400).send(err);
-    })
+    } catch(e){
+        res.status(400).send(e)
+    }
+    
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch(err => {
+    //     res.status(400).send(err);
+    // })
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch(e => {
+    } catch(e){
         res.status(500).send();
-    })
+    }
+
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch(e => {
+    //     res.status(500).send();
+    // })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User.findById(_id).then((user) => {
-        if(!user) return res.status(404).send()
+
+    try {
+        const user = await User.findById(_id);
+        if(!user) res.status(404).send()
         res.send(user)
-    }).catch(e => res.status(500).send())
+    } catch(e){
+        res.status(500).send();
+    }
+
+    // User.findById(_id).then((user) => {
+    //     if(!user) return res.status(404).send()
+    //     res.send(user)
+    // }).catch(e => res.status(500).send())
 })
 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => res.status(201).send(task))
-    .catch((err) => {
-        res.status(400).send(err);
-    })
+    try{
+        await task.save();
+        res.status(201).send(task)
+    } catch(e){
+        res.status(400).send(e)
+    }
+
+    // task.save().then(() => res.status(201).send(task))
+    // .catch((err) => {
+    //     res.status(400).send(err);
+    // })
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then(tasks => {
+app.get('/tasks', async (req, res) => {
+
+    try{
+        const tasks = await Task.find({});
         res.send(tasks)
-    }).catch(e => res.status(500).send());
+    } catch(e){
+        res.status(500).send();
+    }
+
+    // Task.find({}).then(tasks => {
+    //     res.send(tasks)
+    // }).catch(e => res.status(500).send());
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
-    Task.findById(_id).then(task => {
+
+    try{
+        const task = await Task.findById(_id);
         if(!task) res.status(404).send();
         res.send(task);
-    }).catch(e => res.status(500).send())
+    } catch(e){
+        res.status(500).send();
+    }
+
+    // Task.findById(_id).then(task => {
+    //     if(!task) res.status(404).send();
+    //     res.send(task);
+    // }).catch(e => res.status(500).send())
 })
 
 
