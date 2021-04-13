45af0d0047b506a8e5256e9ceed88a0b5f746266
vsvijay987 <vsvijju987@gmail.com>
2021-04-13 22:37:17 +0530

    Resource deleting endpoints

--- src/index.js
+++ src/index.js
@@ -85,6 +85,18 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(e){
+        res.status(500).send();
+    }
+})
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -150,6 +162,18 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task);
     } catch(e) {
         res.status(400).send(e);
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(500).send()
     }
 })
 
