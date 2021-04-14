a5e92d97108c521ca0ae3d7d8409d65ddd1ba7f1
vsvijay987 <vsvijju987@gmail.com>
2021-04-14 17:46:52 +0530

    Acceptingg authentication token

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const User = require("../models/user");
+const auth = require("../middleware/auth")
 
 const router = new express.Router();
 
@@ -32,13 +33,8 @@ router.post("/users/login", async (req, res) => {
   }
 })
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user)
 
   // User.find({}).then((users) => {
   //     res.send(users)
