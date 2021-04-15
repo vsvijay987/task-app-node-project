1905c0b265bcb048cfea9f824d54233ff2023ea0
vsvijay987 <vsvijju987@gmail.com>
2021-04-15 17:47:57 +0530

    Adding Images to user profiles

--- src/routers/user.js
+++ src/routers/user.js
@@ -109,26 +109,31 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: 'avatars',
 
   limits: {
     fileSize: 1000000
   },
 
   fileFilter(req, file, cb) {
-
     if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
       return cb(new Error('Please upload image file!'))
     }
     cb(undefined, true)
-
   }
 });
 
-router.post("/users/me/avatar", upload.single('avatar') , (req, res) => {
+router.post("/users/me/avatar", auth, upload.single('avatar') , async (req, res) => {
+  req.user.avatar = req.file.buffer;
+  await req.user.save();
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({error: error.message})
+})
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
 })
 
 module.exports = router;
