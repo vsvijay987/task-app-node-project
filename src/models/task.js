058d6cb05c1435a82d879f29ee48d500ceda6a22
vsvijay987 <vsvijju987@gmail.com>
2021-04-15 09:15:29 +0530

    Implementing timestamps for new user and task

--- src/models/task.js
+++ src/models/task.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      default: false,
+      type: Boolean,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  completed: {
-    default: false,
-    type: Boolean,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
+  {
+    timestamps: true,
   }
-});
+);
+
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
--- src/models/user.js
+++ src/models/user.js
@@ -2,62 +2,67 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Task = require('./task');
+const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
-    lowerCase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is not correct!");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age can't have a negative value!");
-      }
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      lowerCase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not correct!");
+        }
+      },
     },
-  },
-  password: {
-    type: String,
-    minLength: 6,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password should not contain password text");
-      }
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age can't have a negative value!");
+        }
+      },
     },
+    password: {
+      type: String,
+      minLength: 6,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password should not contain password text");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
-      }
-    }
-  ]
-})
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.virtual('tasks', {
-  ref: 'Task',
-  localField: '_id',
-  foreignField: 'owner'
-})
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
 
-userSchema.methods.toJSON = function(){
+userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
 
@@ -65,52 +70,51 @@ userSchema.methods.toJSON = function(){
   delete userObject.tokens;
 
   return userObject;
-}
+};
 
 //using jwt for generating tokens while login and signup
-userSchema.methods.generateAuthToken = async function() {
+userSchema.methods.generateAuthToken = async function () {
   const user = this;
 
-  const token = jwt.sign({_id: user._id.toString()}, 'thisismynodeproject',);
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynodeproject");
 
-  user.tokens = user.tokens.concat({token});
+  user.tokens = user.tokens.concat({ token });
 
   await user.save();
 
   return token;
-}
-
+};
 
 //making mongoose query to be used in user router
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({email});
-  if(!user){
-    throw new Error("Unable to login")
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
-  if(!isValidPassword){
-    throw new Error("Unable to login")
+  if (!isValidPassword) {
+    throw new Error("Unable to login");
   }
-  return user
-}
+  return user;
+};
 
 //Hash the plain text password before saving
-userSchema.pre('save', async function(next){
+userSchema.pre("save", async function (next) {
   const user = this;
 
-  if(user.isModified('password')){
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
-  next()
-})
+  next();
+});
 
 //Delete user tasks when user is remover
-userSchema.pre('remove', async function(next){
+userSchema.pre("remove", async function (next) {
   const user = this;
-  await Task.deleteMany({owner: user._id})
-  next()
-})
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
