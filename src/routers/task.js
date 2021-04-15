e6927eef2559a35283669f910ec548a96d49a52f
vsvijay987 <vsvijju987@gmail.com>
2021-04-15 09:46:10 +0530

    Filtering tasks on basis of complete or incomplete

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,9 +25,20 @@ router.post("/tasks", auth, async (req, res) => {
   // })
 });
 
+//GET /tasks?completed=true
 router.get("/tasks", auth, async (req, res) => {
+
+  let match = {};
+
+  if(req.query.completed){
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match
+    }).execPopulate();
     // const tasks = await Task.find({owner: req.user._id});
     res.send(req.user.tasks);
   } catch (e) {
